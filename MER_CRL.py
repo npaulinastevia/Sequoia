f74fe4b30b305337eebf9386701e45f030fcd00b
npaulinastevia <npaulinastevia@gmail.com>
2024-01-22 16:55:18 -0500

    init

--- MER_CRL.py
+++ MER_CRL.py
@@ -1,6 +1,6 @@
 import collections
 import math
-
+from dyna_gym.envs import nscartpole_v0, NSCartPoleV0, NSCartPoleV1,NSCartPoleV2
 import config
 import torch
 import torch.nn as nn
@@ -21,7 +21,11 @@ class CNNQNetwork(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.fc1 = nn.Linear(self._calculate_fc_input_size(input_shape), 512)
-        self.fc2 = nn.Linear(512, num_actions)
+        self.fc2 = nn.Linear(512, config.se_actions)
+        #self.fct4 = nn.Linear(num_actions, config.se_actions)
+        #self.fcv2 = nn.Linear(num_actions, 3)
+        #self.fcv0 = nn.Linear(num_actions, 5)
+        self.num_actions=num_actions
 
 
     def _calculate_fc_input_size(self, input_shape):
@@ -37,12 +41,23 @@ class CNNQNetwork(nn.Module):
         x = torch.relu(self.conv2(x))
         return torch.relu(self.conv3(x))
 
-    def forward(self, x):
+    def forward(self, x,n_actions=None):
+        x=x.to(torch.float32)
         if len(self.input_shape)>2:
             x = self._convolutional_layers(x)
             x = x.view(x.size(0), -1)
         x = torch.relu(self.fc1(x))
-        return self.fc2(x)
+        x=self.fc2(x)
+        return x
+
+        #if n_actions==self.num_actions:
+         #   return x
+        #elif n_actions==5:
+        #    return self.fcv0(x)
+        #elif n_actions==3:
+        #    return self.fcv2(x)
+        #else:
+         #   return self.fct4(x)
 
 
 class ExperienceReplayBuffer:
@@ -101,7 +116,18 @@ class MER_CRL:
         self.q_target_network.load_state_dict(self.q_network.state_dict())
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=alpha)
         self.frame_idx=0
-
+        f = 'results.txt'
+        f = open('results.txt', 'a+')
+        f.write('n_actions,x1,x2,x3,x4,rew'+ '\n')
+        f.close()
+        self.task = {
+            10: NSCartPoleV0(),
+            17: NSCartPoleV2(),
+            #64: {"gravity": 10, "length": 0.2},
+            #150: dict(length=0.1,gravity=9.8),
+            #200: dict(length=0.2, gravity=-12.0),
+            #250: dict(length=0.5,gravity=0.9),
+        }
         self.task_schedule_cartpole = {
             0: {"gravity": 9.8, "length": 0.5},
             #0: {"gravity": 10, "length": 0.2},
@@ -111,16 +137,16 @@ class MER_CRL:
             #200: dict(length=0.2, gravity=-12.0),
             #250: dict(length=0.5,gravity=0.9),
         }
-
     def deep_q_learning_test(self):
 
         #theta = torch.rand((1,), requires_grad=True)
 
-        filename = 'bug_log_RELINE.txt'
+        f = 'bug_log_RELINE.txt'
         f = open('bug_log_RELINE.txt', 'w')
         f.close()
         replay_buffer = ExperienceReplayBuffer(capacity=50000)
         flag_injected_bug_spotted = [False, False]
+        self.env=gym.make('CartPole-v1')
         self.env.gravity = 9.8
         self.env.length = 0.5
         for frame in range(50):
@@ -135,11 +161,17 @@ class MER_CRL:
                     action = self.env.action_space.sample()
                 else:
                     with torch.no_grad():
-                        q_values = self.q_network(torch.from_numpy(state).unsqueeze(0))
-                        action = torch.argmax(q_values).item()
+                        #q_values = self.q_network(torch.from_numpy(state).unsqueeze(0),self.env.action_space.n)
+                        action = self.q_network(torch.from_numpy(state).unsqueeze(0),n_actions=self.env.action_space.n).max(1).indices.view(1, 1)
+                        action=action.item()
+
 
                 next_state, reward, episode_done, _ = self.env.step(action)
-                print(next_state)
+                f = open('results.txt', 'a+')
+                f.write(str(self.env.action_space.n) +','+str(next_state[0]) +','+str(next_state[1])+','+str(next_state[2])+','+str(next_state[3])+','+str(reward )+ '\n')
+                f.write('\n')
+                f.close()
+
                 if -0.5 < next_state[0] < -0.45 and not flag_injected_bug_spotted[0]:
                     reward += 50
                     flag_injected_bug_spotted[0] = True
@@ -170,7 +202,7 @@ class MER_CRL:
                         s, a, r,_, s_next = states[i], actions[i], rewards[i], dones[i], next_states[i]#batch[i]
                         s, s_next=torch.from_numpy(s).requires_grad_(True),torch.from_numpy(s_next).requires_grad_(True)
                         s, s_next = s.unsqueeze(0), s_next.unsqueeze(0)
-                        q_values = self.q_network(s)
+                        q_values = self.q_network(s,n_actions=self.env.action_space.n)
                         q_target_values = self.q_target_network(s_next)
                         y = r + self.gamma * torch.max(q_target_values) if not episode_done else torch.tensor(r,requires_grad=True)
 
@@ -219,7 +251,7 @@ class MER_CRL:
             f.write('\n')
             f.close()
             flag_injected_bug_spotted = [False, False]
-            lines = [line for line in open(filename, 'r')]
+            lines = [line for line in open('bug_log_RELINE.txt', 'r')]
             lines_1k = lines[:1000]
 
             count_0bug = 0
@@ -244,45 +276,59 @@ class MER_CRL:
 
         return self.theta, replay_buffer
     def deep_q_learning(self):
-
         #theta = torch.rand((1,), requires_grad=True)
         self.theta = CNNQNetwork(self.state_size, self.action_size).state_dict()
-
+        print(self.steps)
         replay_buffer = ExperienceReplayBuffer(capacity=50000)
         task_id=0
         total_step=0
         for frame in range(self.frame_limit):
+
+            if frame in self.task.keys():
+                    self.env = self.task[frame]
             state = preprocess_state(self.env.reset())
+            state=state[:4]
+
             episode_done = False
 
             while not episode_done:
-                if task_id == 0:
-                    self.env.gravity = self.task_schedule_cartpole[task_id]['gravity']
-                    self.env.length = self.task_schedule_cartpole[task_id]['length']
-                    state = self.env.reset()
-                    task_id = task_id + 1
-                else:
-                    if task_id < len(list(self.task_schedule_cartpole.keys())):
+                # if task_id == 0:
+                #     self.env.gravity = self.task_schedule_cartpole[task_id]['gravity']
+                #     self.env.length = self.task_schedule_cartpole[task_id]['length']
+                #     state = self.env.reset()
+                #     task_id = task_id + 1
+                # else:
+                #     if task_id < len(list(self.task_schedule_cartpole.keys())):
+                #
+                #         if total_step == sorted(list(self.task_schedule_cartpole.keys()))[task_id]:
+                #             self.env.gravity = self.task_schedule_cartpole[total_step]['gravity']
+                #             self.env.length = self.task_schedule_cartpole[total_step]['length']
+                #             state = self.env.reset()
+                #             task_id = task_id + 1
 
-                        if total_step == sorted(list(self.task_schedule_cartpole.keys()))[task_id]:
-                            self.env.gravity = self.task_schedule_cartpole[total_step]['gravity']
-                            self.env.length = self.task_schedule_cartpole[total_step]['length']
-                            state = self.env.reset()
-                            task_id = task_id + 1
-                self.frame_idx += 1
                 # Exploration-exploitation strategy
-                #epsilon = max(config.EPSILON_FINAL, config.EPSILON_START - self.frame_idx / config.EPSILON_DECAY_LAST_FRAME)
-                epsilon =config.EPSILON_FINAL + (config.EPSILON_START - config.EPSILON_FINAL) * \
-                math.exp(-1. * self.frame_idx / config.EPSILON_DECAY_LAST_FRAME)
-                if random.random() <= epsilon:
-                    action = self.env.action_space.sample()
+                epsilon = max(config.EPSILON_FINAL, config.EPSILON_START - self.frame_idx / config.EPSILON_DECAY_LAST_FRAME)
+                #epsilon =config.EPSILON_FINAL + (config.EPSILON_START - config.EPSILON_FINAL) * \
+                #math.exp(-1. * self.frame_idx / config.EPSILON_DECAY_LAST_FRAME)
+                self.frame_idx += 1
+                ran=random.random()
+
+                if ran <= epsilon:
+                    action = gym.make('CartPole-v1').action_space.sample()
                 else:
                     with torch.no_grad():
-                        q_values = self.q_network(torch.from_numpy(state).unsqueeze(0))
-                        action = torch.argmax(q_values).item()
-
+                        #q_values = self.q_network(torch.from_numpy(state).unsqueeze(0),self.env.action_space.n).max(1).indices.view(1, 1)
+                        action = self.q_network(torch.from_numpy(state).unsqueeze(0),n_actions=self.env.action_space.n).max(1).indices.view(1, 1)
+                        action=action.item()
                 next_state, reward, episode_done, _ = self.env.step(action)
+                f = open('results.txt', 'a+')
+                f.write(str(self.env.action_space.n) +','+str(next_state[0]) +','+str(next_state[1])+','+str(next_state[2])+','+str(next_state[3])+','+str(reward )+ '\n')
+                f.write('\n')
+                f.close()
+
                 next_state = preprocess_state(next_state)
+                next_state = next_state[:4]
+
 
                 # Store transition in replay buffer
                 exp = Experience(state, action, reward, episode_done, next_state)
@@ -309,7 +355,8 @@ class MER_CRL:
                         s, a, r,_, s_next = states[i], actions[i], rewards[i], dones[i], next_states[i]#batch[i]
                         s, s_next=torch.from_numpy(s).requires_grad_(True),torch.from_numpy(s_next).requires_grad_(True)
                         s, s_next = s.unsqueeze(0), s_next.unsqueeze(0)
-                        q_values = self.q_network(s)
+
+                        q_values = self.q_network(s,n_actions=self.env.action_space.n)
                         q_target_values = self.q_target_network(s_next)
                         y = r + self.gamma * torch.max(q_target_values) if not episode_done else torch.tensor(r,requires_grad=True)
 
--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import gym
-env = gym.make('CartPole-v1')
-frame_limit = 10
+from dyna_gym.envs import nscartpole_v0, NSCartPoleV0, NSCartPoleV1,NSCartPoleV2
+env = NSCartPoleV1()#CartPole-v1
+se_actions=2
+frame_limit = 25
 alpha = 0.001
 beta = 0.001
 gamma = 0.99
-steps = 32
-k = 16
+steps = 64
+k = 32
 EQ = 100
   # Adjust exploration-exploitation strategy as needed
 frame_idx = 0
-EPSILON_DECAY_LAST_FRAME = 150000
-EPSILON_START = 1.0
-EPSILON_FINAL = 0.01
+EPSILON_DECAY_LAST_FRAME = 1000
+EPSILON_START = 0.9
+EPSILON_FINAL = 0.05
 epsilon = EPSILON_START
